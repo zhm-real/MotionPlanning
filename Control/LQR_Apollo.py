b1e8903d1d3ca14be3521afa9693c0dae417f20e
zhm-real <zhou.hm0420@gmail.com>
2020-08-13 12:40:10 -0700

    add LQR_Apollo

--- Control/LQR_Apollo.py
+++ Control/LQR_Apollo.py
@@ -13,6 +13,7 @@ import os
 import sys
 import math
 import numpy as np
+from enum import Enum
 import matplotlib.pyplot as plt
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) +
@@ -20,6 +21,45 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)) +
 
 import HybridAstarPlanner.hybrid_astar as HybridAStar
 import HybridAstarPlanner.draw as draw
+from Control.lateral_controller_conf import *
+
+
+class VehicleState:
+    def __init__(self, x, y, yaw, v, gear):
+        self.x = x
+        self.y = y
+        self.yaw = yaw
+        self.v = v
+        self.gear = gear
+
+
+class Gear(Enum):
+    FORWARD = 1
+    BACKWARD = 2
+
+
+class LatController:
+    def __init__(self):
+        self.x = 0
+
+
+def main():
+    x = Gear.BACKWARD
+    if x == Gear.FORWARD:
+        print(2)
+    else:
+        print(1)
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
 
 
 
